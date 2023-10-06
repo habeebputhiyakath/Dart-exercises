c7c8ef4742c6c341de592b3033ec792f1aeff95b
habeebputhiyakath <irfanputhiyakat@gmail.com>
2023-10-06 22:44:10 +0530

    first commit

---
+++ Dart-exercises_set-1.dart
@@ -0,0 +1,69 @@
+// Question: 1
+
+// Print The pattern. EEEEE
+// DDDD
+// CCC
+// BB
+// A
+
+// Question: 2
+
+// Print The pattern. A
+
+// BB
+// CCC
+// DDDD
+// EEEEE
+// Question: 3
+
+// write a code for Guessing Tempurature. * if the guessing is below show and message that Guessing is Lower than
+// temeprature
+// * The guessing is greater than temperature then show an message that
+// Guessing is greater than tempurature
+// * If it correct show your'e Guessing is Correct
+
+
+// Question: 4
+// sort a list of names into another list, only including names with a length greater
+// than 3, List<String> names = ["Alice", "Raju", "Eve", "John", "Dean", "Lia", "Olivia"];
+
+
+// Question: 5
+
+// Write Dart code to calculate the sum of all Positive even numbers in a list. List<int> numbersList = [2, 5, 10, 7, 14, 6, 3,-6,52,-14,11,85,-61,0,-2];
+// Question: 6
+
+// Write a Dart program to display the current date and time. eg: Current date and time : 2023-09-10 13:34:55
+
+
+// Question: 8
+// Write a Dart program which accepts the user's first and last name and print them
+
+// in reverse order with a space between them. Eg : Irfan Habeeb
+// Ans = Habeeb Irfan ;
+
+// Question: 9
+// Write a Dart program to display the rearranged date.
+// Eg: List date = [12,5,2023]. To arrangeddate = 12/5/2023
+
+
+// Question: 10
+// Write Dart code to remove duplicate String elements from the list . List stringsList = [ "apple", "banana", "cherry", "Orange", 2, 4, "banana", 5, 2, "pineapple", "berry", 44, "apple", 68, 2, 31,
+// ];
+
+// Question: 11
+
+// Write Dart code to find the average of all elements in a list of doubles. doubleList = [2.5, 1.5, 3.0, 4.5];
+
+// Questions: 12
+
+// keep track of when our friend’s birthdays are, and be able to find that information
+// based on their name. Map is
+// birthdays = { "Albert ": "14/03/1879", "Habeeb": "20/2/2015", "Benjamin ": "17/01/1706", "Irfan": "6/7/1985", "Franklin": "10/12/1815", };
+// Eg Ans: Irfan's birthday is 6/7/1985
+
+// Questions: 13
+
+// You have a list of integers named numbersList containing various numbers. Write
+// Dart code to filter this list to only include numbers greater than 10 and store the
+// result in a new list. List<int> numbersList = [5, 12, 8, 15, 3, 18, 20, 9,1,55,-10,8,61,-23]
